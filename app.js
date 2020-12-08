779007406c7825ed5cbfa7ab2f76fc96e7418f26
Ivancanet <ivancanyet96@gmail.com>
2020-12-08 01:29:48 +0100

    making the login

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 require('dotenv').config();
 
-const bodyParser   = require('body-parser');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
-const mongoose     = require('mongoose');
-const logger       = require('morgan');
-const path         = require('path');
+const express = require('express');
+const favicon = require('serve-favicon');
+const hbs = require('hbs');
+const mongoose = require('mongoose');
+const logger = require('morgan');
+const path = require('path');
 
 
 mongoose
-  .connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.1b6hf.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`, {useNewUrlParser: true, useUnifiedTopology: true})
+  .connect(`mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.1b6hf.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
@@ -33,18 +33,23 @@ app.use(cookieParser());
 // Express View engine setup
 
 app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
+  src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
   sourceMap: true
 }));
-      
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
-
+//CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  next();
+});
 
 // default value for title local
 app.locals.title = 'Express - Generated with IronGenerator';
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,86 @@
 const express = require('express');
-const router  = express.Router();
+const app = require('../app');
+const router = express.Router();
+const Admin = require('../models/Admin');
 
-/* GET home page */
+//#region Home Page
+
+// Get of Home Page
 router.get('/', (req, res, next) => {
-  res.render('index');
+  res.send('Home')
 });
+
+//#endregion
+
+//#region Blog Page
+
+//#endregion
+
+//#region Restaurants Page
+
+//#endregion
+
+//#region Wines Page
+
+//#endregion
+
+//#region Formations Page
+
+//#endregion
+
+//#region Experiencies Page
+
+//#endregion
+
+//#region About us Page
+
+//#endregion
+
+//#region Contact Page
+
+//#endregion
+
+//#region Admin Page (Log In)
+
+router.post('/admin', (req, res, next) => {
+
+  const { username, password } = req.body;
+
+  /* Test -->  */ //res.send(`Username: ${username}, Password: ${password}`);
+
+  Admin.findOne({ username })
+    .then((result => {
+
+      if (result){
+        password === result.password ? res.send('Loggged In') : res.send('Incorrect Password')
+      }
+      
+      
+    }))
+    .catch((err) => {
+      res.send(err);
+    })
+})
+
+//#endregion
+
+
+
+//#region Create admins (Only developer)
+
+//Post to create admins
+
+router.post('/new-user', (req, res, next) => {
+  Admin.create(req.body)
+    .then((result) => {
+      res.send(result)
+    })
+    .catch((err) => {
+      res.send(err)
+    });
+
+});
+
+//#endregion
 
 module.exports = router;
